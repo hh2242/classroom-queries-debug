ca4efd6e25cb4439bdf325941b00fdc2277ba797
hh2242 <165195157+hh2242@users.noreply.github.com>
2024-05-07 23:39:26 +0000

    finished

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,12 @@
 class DepartmentsController < ApplicationController
   def index
     @departments = Department.all.order({ :created_at => :desc })
-
     render({ :template => "departments/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
+    @department = Department.where({:id => the_id }).first
 
     render({ :template => "departments/show" })
   end
